08e55d707048ed56da919285daccb01b5217c919
aforfree <aforfree@gmail.com>
2022-12-04 18:50:31 +0500

    Stepik lesson 4.2 step 9: minor changes

--- test_main_page.py
+++ test_main_page.py
@@ -3,11 +3,8 @@ from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.should_be_login_link()
+    page = MainPage(browser, link) 
+    page.open() 
     page.go_to_login_page()
-
-    login_link = browser.current_url
-    login_page = LoginPage(browser, login_link)
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
